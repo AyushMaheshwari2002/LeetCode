d5f76bb4a9861bceb82587f71ac2ea4df1c0f51b
AyushMaheshwari2002 <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-01-22 19:24:55 +0530

    Create 0206. Reverse Linked List.cpp

---
+++ 0206. Reverse Linked List.cpp
@@ -0,0 +1,35 @@
+/*  Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+        Example 1:
+                Input: head = [1,2,3,4,5]
+                Output: [5,4,3,2,1]
+
+        Example 2:
+                Input: head = [1,2]
+                Output: [2,1]
+
+        Example 3:
+                Input: head = []
+                Output: []
+*/
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        ListNode* previous = NULL;
+        ListNode* current = head;
+        ListNode* next;
+      
+        while(current != NULL)
+        {
+            next = current -> next;
+            current -> next = previous;
+            
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+};
