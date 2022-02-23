5011270ba3b5b086f8aabe1a4a7d09b4ee2b8628
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-02-23 19:10:43 +0530

    Create 0228. Summary Ranges.cpp

---
+++ 0228. Summary Ranges.cpp
@@ -0,0 +1,56 @@
+/* You are given a sorted unique integer array nums.
+   Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+   Each range [a,b] in the list should be output as:
+   "a->b" if a != b
+   "a" if a == b
+ 
+              Example 1:
+              Input: nums = [0,1,2,4,5,7]
+              Output: ["0->2","4->5","7"]
+              Explanation: The ranges are:
+              [0,2] --> "0->2"
+              [4,5] --> "4->5"
+              [7,7] --> "7"
+             
+              Example 2:
+              Input: nums = [0,2,3,4,6,8,9]
+              Output: ["0","2->4","6","8->9"]
+              Explanation: The ranges are:
+              [0,0] --> "0"
+              [2,4] --> "2->4"
+              [6,6] --> "6"
+              [8,9] --> "8->9"
+*/
+
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) 
+    {    
+        vector<string> result;
+        int n = nums.size();
+        
+        if(n == 0)
+            return result;
+        
+        int start = nums[0];
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(i == n-1 || nums[i]+1 != nums[i+1])
+            {
+                if(nums[i] != start)
+                    result.push_back(to_string(start) + "->" + to_string(nums[i]));
+                
+                else
+                    result.push_back(to_string(start));
+            
+                if(i != n-1)
+                    start = nums[i+1];
+            }
+        }
+        return result;
+    }
+};
+
+
