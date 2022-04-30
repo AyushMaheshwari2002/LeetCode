49e7da8c321bec3a5a4365fb8b923cbc0633f700
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-04-30 22:13:03 +0530

    Create 1370. Increasing Decreasing String.cpp

---
+++ 1370. Increasing Decreasing String.cpp
@@ -0,0 +1,66 @@
+/*  You are given a string s. Reorder the string using the following algorithm:
+          * Pick the smallest character from s and append it to the result.
+          * Pick the smallest character from s which is greater than the last appended character to the result and append it.
+          * Repeat step 2 until you cannot pick more characters.
+          * Pick the largest character from s and append it to the result.
+          * Pick the largest character from s which is smaller than the last appended character to the result and append it.
+          * Repeat step 5 until you cannot pick more characters.
+          * Repeat the steps from 1 to 6 until you pick all characters from s.
+          * In each step, If the smallest or the largest character appears more than once you can choose any occurrence and append it to the result.
+    Return the result string after sorting s with this algorithm.
+
+                Example 1:
+                Input: s = "aaaabbbbcccc"
+                Output: "abccbaabccba"
+                Explanation: After steps 1, 2 and 3 of the first iteration, result = "abc"
+                After steps 4, 5 and 6 of the first iteration, result = "abccba"
+                First iteration is done. Now s = "aabbcc" and we go back to step 1
+                After steps 1, 2 and 3 of the second iteration, result = "abccbaabc"
+                After steps 4, 5 and 6 of the second iteration, result = "abccbaabccba"
+
+                Example 2:
+                Input: s = "rat"
+                Output: "art"
+                Explanation: The word "rat" becomes "art" after re-ordering it with the mentioned algorithm.
+*/
+
+
+
+class Solution {
+public:
+    string sortString(const string& s) 
+    {
+        string res = "";
+        vector<int> v(26, 0);
+        
+        for(int i = 0; i < s.size(); i++) 
+        {
+            v[s[i] - 'a']++;
+        }
+        
+        while(res.size() < s.size()) 
+        {    
+            for(int i = 0; i < v.size(); i++)
+            {
+                if(v[i] > 0) 
+                {
+                    res += i + 'a'; 
+                    v[i]--;
+                }
+            }
+            
+            for(int i = v.size()-1; i >= 0; i--)
+            {
+                if(v[i] > 0) 
+                {
+                    res += i + 'a'; 
+                    v[i]--;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+
+
