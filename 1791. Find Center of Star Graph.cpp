fc12017c0866966461260410f43265c907311066
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-05-04 23:20:39 +0530

    Update 1791. Find Center of Star Graph.cpp

--- 1791. Find Center of Star Graph.cpp
+++ 1791. Find Center of Star Graph.cpp
@@ -4,13 +4,13 @@
     given star graph.
 
               Example 1:
-              Input: edges = [[1,2],[2,3],[4,2]]
-              Output: 2
-              Explanation: As shown in the figure above, node 2 is connected to every other node, so 2 is the center.
+                      Input: edges = [[1,2],[2,3],[4,2]]
+                      Output: 2
+                      Explanation: As shown in the figure above, node 2 is connected to every other node, so 2 is the center.
 
               Example 2:
-              Input: edges = [[1,2],[5,1],[1,3],[1,4]]
-              Output: 1
+                      Input: edges = [[1,2],[5,1],[1,3],[1,4]]
+                      Output: 1
 */
 
 
