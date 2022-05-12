d330552b4ea2c94ada7326a56edc5efea3828939
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-05-12 23:36:22 +0530

    Update 1721. Swapping Nodes in a Linked List.cpp

--- 1721. Swapping Nodes in a Linked List.cpp
+++ 1721. Swapping Nodes in a Linked List.cpp
@@ -12,3 +12,46 @@
 
 
 
+class Solution {
+public:
+    
+    void swap(int i, int j, vector<int> &v)
+    {
+        int temp = v[i];
+        v[i] = v[j];
+        v[j] = temp;
+    }
+    
+    ListNode* swapNodes(ListNode* head, int k) 
+    {
+        ListNode* temp = head;
+        ListNode* temp2 = head;
+        
+        // store the linked list in the form of vector
+        vector<int> v;
+        int count = 0;              //this will give the size of linked list
+        
+        while(temp != NULL)
+        {
+            v.push_back(temp -> val);
+            temp = temp -> next;
+            count++;
+        }
+        
+        swap(k - 1 , count - k , v);
+        
+        // rebuilt the linked list
+        int i = 0;
+        while(temp2 != NULL)
+        {
+            temp2 -> val = v[i];
+            i++;
+            temp2 = temp2 -> next;
+        }
+        
+        return head;
+    }
+};
+
+
+
