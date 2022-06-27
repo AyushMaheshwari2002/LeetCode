cd01bf5f5c928d95adae6086af145559b2f90213
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-06-27 21:02:41 +0530

    Update 2169. Count Operations to Obtain Zero.cpp

--- 2169. Count Operations to Obtain Zero.cpp
+++ 2169. Count Operations to Obtain Zero.cpp
@@ -25,3 +25,52 @@
 
 
 
+// ITERATIVE APPROACH
+class Solution {
+public:
+    int countOperations(int num1, int num2) 
+    {
+        int count = 0;
+        
+        while(num1 != 0 && num2 != 0)
+        {
+            if(num1 >= num2)
+            {
+                num1 = num1 - num2;
+                count++;
+            }
+            else
+            {
+                num2 = num2 - num1;
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
+
+
+
+/*
+// RECURSIVE APPROACH
+class Solution {
+public:
+	int countOperations(int num1, int num2) 
+    {
+		if(num1 == 0 || num2 == 0)
+        {
+			return 0;
+		}
+		else if(num1 >= num2)
+        {
+			return 1 + countOperations(num1 - num2, num2);
+		}
+        
+		return 1 + countOperations(num1, num2 - num1);
+	}
+};
+*/
+
+
+
