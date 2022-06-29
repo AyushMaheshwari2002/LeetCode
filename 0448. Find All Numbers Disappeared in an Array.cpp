9752885617eea3b418b05463a288a9583971adb1
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-06-29 22:22:47 +0530

    Update 0448. Find All Numbers Disappeared in an Array.cpp

--- 0448. Find All Numbers Disappeared in an Array.cpp
+++ 0448. Find All Numbers Disappeared in an Array.cpp
@@ -11,3 +11,27 @@
 
 
 
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums)
+    {
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            mp[nums[i]]++;
+        }
+        
+        for(int i = 1; i <= nums.size(); i++)
+        {
+            if(mp[i] == 0)
+                ans.push_back(i);
+        }
+        
+        return ans;
+    }
+};
+
+
+
