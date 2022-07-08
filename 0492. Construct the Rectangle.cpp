df75bcc73cbe9b613ffffd6fcdb7aee70f3ad3d0
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-07-08 22:50:55 +0530

    Update 0492. Construct the Rectangle.cpp

--- 0492. Construct the Rectangle.cpp
+++ 0492. Construct the Rectangle.cpp
@@ -23,3 +23,19 @@
 
 
 
+class Solution {
+public:
+    vector<int> constructRectangle(int area) 
+    {
+        int m = sqrt(area);
+        
+        while (area % m != 0)
+        {
+            m--;
+        }
+        return {area/m , m};
+    }
+};
+
+
+
