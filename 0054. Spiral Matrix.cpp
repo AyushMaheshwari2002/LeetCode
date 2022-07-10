eb58380f29d0f7b16dbf77ead58ecf2b7cf4b689
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-07-10 23:12:34 +0530

    Update 0054. Spiral Matrix.cpp

--- 0054. Spiral Matrix.cpp
+++ 0054. Spiral Matrix.cpp
@@ -11,3 +11,70 @@
 
 
 
+class Solution {
+public:
+    
+    vector<int> spiralOrder(vector<vector<int>>& matrix) 
+    {
+        vector<int> v;       // to store the resulting array
+		
+        int top = 0;
+        int bottom = matrix.size() - 1;
+        int left = 0;
+        int right = matrix[0].size() - 1;
+		
+	//initializing direction
+        int direction = 0; 
+
+        while(top <= bottom && left <= right)
+        {	
+            if(direction == 0)
+            {
+	          //left -> right
+                for(int i = left; i <= right; i++)
+                {
+                    v.push_back(matrix[top][i]);
+                }
+                top++;
+            }
+            
+            else if(direction == 1)
+            {
+		//top -> bottom
+                for(int i = top; i <= bottom; i++)
+                {
+                    v.push_back(matrix[i][right]);
+                }
+                right--;
+            }
+            
+            else if(direction == 2)
+            {
+		//right -> left
+                for(int i = right; i >= left; i--)
+                {
+                    v.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            
+            else if(direction == 3)
+            {
+		//bottom -> top
+                for(int i = bottom; i >= top; i--)
+                {
+                    v.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+			
+	  //increment the value of direction
+            direction = (direction + 1) % 4;
+        }
+
+        return v;
+    }
+};
+
+
+
