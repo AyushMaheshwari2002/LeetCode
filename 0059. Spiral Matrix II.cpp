52e1b0c879c7fdc293fe051415f690f1092aef80
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-07-10 23:17:54 +0530

    Update 0059. Spiral Matrix II.cpp

--- 0059. Spiral Matrix II.cpp
+++ 0059. Spiral Matrix II.cpp
@@ -11,3 +11,68 @@
 
 
 
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n)
+    {
+        // create n * n vector of vectors we will populate and return at the end
+        vector<vector<int>> v(n, vector<int>(n));
+        
+        int top = 0 , bottom = n - 1 , left = 0 , right = n - 1;
+		
+	//initializing direction
+        int direction = 0; 
+        int val = 1;
+
+        while(top <= bottom && left <= right)
+        {	
+            if(direction == 0)
+            {
+		//left -> right
+                for(int i = left; i <= right; i++)
+                {
+                    v[top][i] = val++;
+                }
+                top++;
+            }
+            
+            else if(direction == 1)
+            {
+		//top -> bottom
+                for(int i = top; i <= bottom; i++)
+                {
+                    v[i][right] = val++;
+                }
+                right--;
+            }
+            
+            else if(direction == 2)
+            {
+		//right -> left
+                for(int i = right; i >= left; i--)
+                {
+                    v[bottom][i] = val++;
+                }
+                bottom--;
+            }
+            
+            else if(direction == 3)
+            {
+		//bottom -> top
+                for(int i = bottom; i >= top; i--)
+                {
+                    v[i][left] = val++;
+                }
+                left++;
+            }
+			
+		//increment the value of direction
+            direction = (direction + 1) % 4;
+        }
+
+        return v;
+    }
+};
+
+
+
