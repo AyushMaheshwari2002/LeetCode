72cdfd9fbb8f7788290664e1cbddd7162f5129c4
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-07-12 16:34:46 +0530

    Update 0022. Generate Parentheses.cpp

--- 0022. Generate Parentheses.cpp
+++ 0022. Generate Parentheses.cpp
@@ -11,3 +11,41 @@
 
 
 
+class Solution {
+public:
+    vector<string> res;
+    
+    void helper(int open, int close, int n, string current)
+    {
+        /* OR WE CAN USE THIS CONDITION TO STOP RECURSION
+        if(open == n && close == n)
+        {
+            result.push_back(current);
+            return;
+        }
+        */
+        
+        if(current.length() == n*2)
+        {
+            res.push_back(current);
+            return;
+        }
+        
+        if(open < n)  
+            helper(open+1 , close , n , current+"(");
+        
+        if(close < open)
+            helper(open , close+1 , n , current+")");
+    }
+    
+    vector<string> generateParenthesis(int n)
+    {
+        helper(0 , 0 , n , "");
+        
+        return res;
+    }
+};
+
+
+
+
