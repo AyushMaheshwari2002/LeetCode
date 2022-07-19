ce02860b45adfe71c9d55901a10b53aec9bad3ce
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-07-19 20:32:45 +0530

    Update 1249. Minimum Remove to Make Valid Parentheses.cpp

--- 1249. Minimum Remove to Make Valid Parentheses.cpp
+++ 1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -1,5 +1,5 @@
 /*  Given a string s of '(' , ')' and lowercase English characters.
-Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+  Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
     Formally, a parentheses string is valid if and only if:
             * It is the empty string, contains only lowercase characters, or
             * It can be written as AB (A concatenated with B), where A and B are valid strings, or
@@ -20,6 +20,39 @@ Your task is to remove the minimum number of parentheses ( '(' or ')', in any po
                         Explanation: An empty string is also valid.
 */
 
+
+
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s)
+    {
+        stack<int> stack;
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(')
+                stack.push(i);
+            
+            else if(s[i] == ')')
+            {
+                if(!stack.empty() && s[stack.top()] == '(')
+                    stack.pop();
+                else
+                    stack.push(i);
+            }
+        }
+        
+        while(!stack.empty())
+        {
+            // we have only earse 1 character so (stack.top() , 1) here 1 is the length of the character which we have erase from original string "s".
+            s.erase(stack.top() , 1); 
+            stack.pop();
+        } 
+        
+        return s;
+    }
+};
 
 
 
