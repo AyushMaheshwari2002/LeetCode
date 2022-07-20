da90f87d366ea422f3326e29fe0f74a43ed33d1c
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-07-20 21:48:26 +0530

    Update 0128. Longest Consecutive Sequence.cpp

--- 0128. Longest Consecutive Sequence.cpp
+++ 0128. Longest Consecutive Sequence.cpp
@@ -13,3 +13,34 @@
 
 
 
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums)
+    {
+        unordered_set<int> s;
+        int ans = 0;
+
+        for(int i : nums)
+            s.insert(i);
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(s.find(nums[i]-1) == s.end())        //run if previous element is not found
+            {
+                int count = 1;
+                
+                while(s.find(nums[i] + count) != s.end())          //runs if next element is found
+                {
+                    count++;
+                }
+                
+                ans = max(ans , count);
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+
