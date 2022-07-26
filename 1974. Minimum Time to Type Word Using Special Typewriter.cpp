d2280827676e5e7a1fd23f99919660546832d569
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-07-26 22:42:35 +0530

    Update 1974. Minimum Time to Type Word Using Special Typewriter.cpp

--- 1974. Minimum Time to Type Word Using Special Typewriter.cpp
+++ 1974. Minimum Time to Type Word Using Special Typewriter.cpp
@@ -47,3 +47,22 @@
 
 
 
+class Solution {
+public:
+    int minTimeToType(string word)
+    {
+        int res = word.size(), pointer = 'a';
+        
+        for (auto ch : word) 
+        {
+            // first check in clockwise then in anticlockwise and take minnimum of that. 
+            res += min(abs(ch - pointer), 26 - abs(pointer - ch));
+            pointer = ch;
+        }
+        
+        return res;
+    }
+};
+
+
+
