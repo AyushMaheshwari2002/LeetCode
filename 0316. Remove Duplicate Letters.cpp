bae799f4df2651342f5124fac509d438d2e04a55
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-03 22:41:58 +0530

    Update 0316. Remove Duplicate Letters.cpp

--- 0316. Remove Duplicate Letters.cpp
+++ 0316. Remove Duplicate Letters.cpp
@@ -13,3 +13,41 @@
 
 
 
+class Solution {
+public:
+    string removeDuplicateLetters(string s)
+    {
+        vector<int> last(26);    // this will store the last occurrence of every character
+        vector<bool> visited(26);     // this will check that we visit that character or not
+        
+        string ans = "";
+        
+        // this loop will count the last occurrence
+        for(int i = 0; i < s.size(); i++)
+        {
+            last[s[i] - 'a'] = i;
+        }
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            // if we already visited that character then we will continue the loop without adding it.
+            if(visited[s[i] - 'a'])
+                continue;
+            
+            while(!ans.empty() && ans.back() > s[i] && last[ans.back() - 'a'] > i)
+            {
+                visited[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+            
+            ans.push_back(s[i]);
+            visited[s[i] - 'a'] = true;
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
