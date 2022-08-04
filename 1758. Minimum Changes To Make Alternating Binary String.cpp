c6d6294f66f68d3c77214dc6f3039cf13e7fa8f4
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-04 23:38:09 +0530

    Update 1758. Minimum Changes To Make Alternating Binary String.cpp

--- 1758. Minimum Changes To Make Alternating Binary String.cpp
+++ 1758. Minimum Changes To Make Alternating Binary String.cpp
@@ -21,3 +21,35 @@
 
 
 
+class Solution {
+public:
+    int minOperations(string s)
+    {
+        int cnt1 = 0;    // count if starting if from 0 
+        int cnt2 = 0;    // count if starting is from 1
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(i % 2 == 0)
+            {
+                if(s[i] == '1')
+                    cnt1++;
+                else
+                    cnt2++;
+            }
+            else
+            {
+                if(s[i] == '0')
+                    cnt1++;
+                else
+                    cnt2++;
+            }
+        }
+        
+        return min(cnt1 , cnt2);
+    }
+};
+
+
+
+
