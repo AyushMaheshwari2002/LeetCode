34e2730c36e7f448dd08703a0eb4ff75d973d136
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-05 23:36:04 +0530

    Update 0895. Maximum Frequency Stack.cpp

--- 0895. Maximum Frequency Stack.cpp
+++ 0895. Maximum Frequency Stack.cpp
@@ -31,3 +31,45 @@
 
 
 
+class FreqStack {
+public:
+    // this will store the frequency of element
+    unordered_map<int,int> freq;
+    // we create multiple stacks for different values of frequency
+    // But the element that come last will come first of same count
+    unordered_map<int,stack<int>> group_stack;
+    
+    int maxfreq = 0;
+    
+    FreqStack() {}
+    
+    void push(int val)
+    {
+        // increment the frequency 
+        freq[val]++;
+        // if element occurs many times then update the value of max_freq(store the maximum frequency of element)
+        maxfreq = max(maxfreq , freq[val]);
+        // store the element in stack according to its frequency
+        group_stack[freq[val]].push(val);     
+    }
+    
+    int pop() 
+    {
+        // find the maximum frequency element in stack
+        int top = group_stack[maxfreq].top();
+        // remove that element from stack
+        group_stack[maxfreq].pop();
+        // decrement its count from frequency map
+        freq[top]--;
+        // if there is no element in stack for that frequency then decrement maxfreq
+        if(group_stack[maxfreq].size() == 0)
+            maxfreq--;
+        
+        return top;
+        
+    }
+};
+
+
+
+
