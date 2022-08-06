2cb160cae307dd27c46fd1d8875c9f99435e7db3
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-06 23:09:18 +0530

    Update 0046. Permutations.cpp

--- 0046. Permutations.cpp
+++ 0046. Permutations.cpp
@@ -16,3 +16,23 @@
 
 
 
+//USING STL
+class Solution {
+public:
+    
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+        vector<vector<int>> ans;
+        
+        sort(nums.begin(),nums.end());
+        
+        do {
+            ans.push_back(nums);
+        }while(next_permutation(nums.begin(),nums.end()));
+        
+        return ans;
+    }
+};
+
+
+
