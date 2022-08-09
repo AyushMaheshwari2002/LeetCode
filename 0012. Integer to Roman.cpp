bb3b17df91bc517be73bf54f66fd1e7b3724e174
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-09 23:25:04 +0530

    Update 0012. Integer to Roman.cpp

--- 0012. Integer to Roman.cpp
+++ 0012. Integer to Roman.cpp
@@ -35,3 +35,45 @@
 
 
 
+class Solution {
+public:
+    string intToRoman(int num)
+    {
+        int val[13] = {1000 , 900 , 500 , 400 , 100 , 90 , 50 , 40 , 10 , 9 , 5 , 4 , 1};
+        string roman[13] = {"M" , "CM" , "D" , "CD" , "C" , "XC" , "L" , "XL" , "X" , "IX" , "V" , "IV" , "I"};
+        
+        string ans = "";
+        
+        for(int i = 0; i < 13; i++)
+        {
+            while(num >= val[i])
+            {
+                ans += roman[i];
+                num -= val[i];
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
+/* TRICK FOR SOLVING THESE QUESTIONS
+class Solution {
+public:
+    string intToRoman(int num)
+    {
+        string ones[] = {"" , "I" , "II" , "III" , "IV" , "V" , "VI" , "VII" , "VIII" , "IX"};
+        string tens[] = {"" , "X" , "XX" , "XXX" , "XL" , "L" , "LX" , "LXX" , "LXXX" , "XC"};
+        string hnds[] = {"" , "C" , "CC" , "CCC" , "CD" , "D" , "DC" , "DCC" , "DCCC" , "CM"};
+        string ths[]  = {"" , "M" , "MM" , "MMM"};
+        
+        return ths[num/1000] + hnds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+    }
+};
+*/
+
+
+
