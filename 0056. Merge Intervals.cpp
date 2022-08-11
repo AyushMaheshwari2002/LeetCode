779630dba2d3d011b79187995c11b9c346ff1d9f
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-11 23:28:45 +0530

    Update 0056. Merge Intervals.cpp

--- 0056. Merge Intervals.cpp
+++ 0056. Merge Intervals.cpp
@@ -14,3 +14,37 @@
 
 
 
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals)
+    {
+        sort(intervals.begin() , intervals.end());
+        
+        vector<vector<int>> ans;
+        int start = intervals[0][0] , end = intervals[0][1];
+        
+        for(auto i : intervals)
+        {
+            // no overlapping
+            // here i contains 2 values like [1,3] so we use i[0] and i[1]
+            if(i[0] > end)
+            {
+                ans.push_back({start,end});
+                // update start and end
+                start = i[0];
+                end = i[1];
+            }
+            // overlapping
+            else
+            {
+                end = max(end , i[1]);
+            }
+        }
+        ans.push_back({start,end});
+            
+        return ans;
+    }
+};
+
+
+
