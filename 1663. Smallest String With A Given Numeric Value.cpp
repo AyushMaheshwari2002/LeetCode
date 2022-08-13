593e856232bf140ed8285d0baf8b2f49eef487ad
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-13 23:23:46 +0530

    Update 1663. Smallest String With A Given Numeric Value.cpp

--- 1663. Smallest String With A Given Numeric Value.cpp
+++ 1663. Smallest String With A Given Numeric Value.cpp
@@ -19,3 +19,55 @@
 
 
 
+class Solution {
+public:
+    string getSmallestString(int n, int k) 
+    {
+        //make a string of lenght n which contains all 'a'
+        string s(n , 'a');
+        
+        k = k - n;
+        
+        while(k > 0)
+        {
+            s[--n] += min(25 , k);
+            k -= min(25 , k);
+        }
+        
+        return s;
+    }
+};
+
+
+/*
+DRY RUN
+if n = 5 , k = 73
+
+Step 1 : aaaaa    (make a string of lenght n with all 'a')
+Step 2 : 68        (k = k - n)
+Step 3 : while(k > 0) run the loop
+
+        1st Iteration
+             s[4] += 25                       (s[4] += min(25 , 68)
+             s[4] = 'z'                       (s[4] = 'a' + min(25,68) = 'a' + 25 = 26 ==> 'z') 
+             k = 43                           (k = k - min(25,68) => 68 - 25 ==> 43) 
+        String becomes aaaaz  and k = 43
+                                    
+        2nd Iteration
+             s[3] += 25                       (s[3] += min(25 , 43)
+             s[3] = 'z'                       (s[3] = 'a' + min(25,43) = 'a' + 25 = 26 ==> 'z')
+             k = 18                           (k = k - min(25,43) => 43 - 25 ==> 18) 
+        String becomes aaazz  and k = 18
+                                               
+        3rd Iteration
+             s[2] += 18                       (s[2] += min(25 , 18)
+             s[3] = 's'                       (s[2] = 'a' + min(25,18) = 'a' + 18 = 19 ==> 's')
+             k = 0                            (k = k - min(25,18) => 18 - 18 ==> 0) 
+        String becomes aaszz  and k = 0
+        
+Step 4: Output : aaaszz                        (return s)
+*/
+
+
+
+
