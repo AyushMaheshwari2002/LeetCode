df251ac779634529e3479d2cda9e298cec00e61c
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-15 22:41:23 +0530

    Update 0096. Unique Binary Search Trees.cpp

--- 0096. Unique Binary Search Trees.cpp
+++ 0096. Unique Binary Search Trees.cpp
@@ -12,3 +12,28 @@
 
 
 
+class Solution {
+public:
+    int numTrees(int n)
+    {
+        if(n < 2) 
+            return 1;
+        
+        // take n+1 b'coz there will be a possibility of n = 0
+        vector<int> v(n+1, 0);
+        v[0] = 1, v[1] = 1;
+        
+        for(int i = 2; i <= n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                // Catalan's Number Formula
+                v[i] += v[j] * v[i-j-1];
+            }
+        }
+        return v[n];
+    }
+};
+
+
+
