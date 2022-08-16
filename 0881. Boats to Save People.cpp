80346090a4e39786b472213e21e45c48fe6ee80c
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-16 23:02:25 +0530

    Update 0881. Boats to Save People.cpp

--- 0881. Boats to Save People.cpp
+++ 0881. Boats to Save People.cpp
@@ -21,3 +21,35 @@
 
 
 
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) 
+    {
+        sort(people.begin() , people.end());
+        
+        int left = 0;
+        int right = people.size()-1;
+        int boats = 0;
+        
+        while(left <= right)
+        {
+            // both will come together
+            if(people[left] + people[right] <= limit)
+            {
+                left++;
+                right--;
+            }
+            else
+            {
+                // only heaviest weight people will come
+                right--;
+            }
+            boats++;
+        }
+        
+        return boats;
+    }
+};
+
+
+
