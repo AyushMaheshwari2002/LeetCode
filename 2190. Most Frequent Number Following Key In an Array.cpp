84311b7ba45039b92d0d07b9f503e5faef747f8c
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-18 23:56:40 +0530

    Update 2190. Most Frequent Number Following Key In an Array.cpp

--- 2190. Most Frequent Number Following Key In an Array.cpp
+++ 2190. Most Frequent Number Following Key In an Array.cpp
@@ -24,3 +24,32 @@
 
 
 
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) 
+    {
+        map<int , int> m;
+        int ans , freq = 0;
+        
+        for(int i = 0; i < nums.size()-1; i++)
+        {
+            if(nums[i] == key)
+            {
+                m[nums[i+1]]++;
+            }
+        }
+        for(auto i : m)
+        {
+            if(i.second > freq)
+            {
+                ans = i.first;
+                freq = i.second;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+
