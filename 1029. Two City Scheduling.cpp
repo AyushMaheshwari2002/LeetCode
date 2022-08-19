dd34a3ba89613b5c2a402abf99548811720a4477
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-19 23:45:10 +0530

    Update 1029. Two City Scheduling.cpp

--- 1029. Two City Scheduling.cpp
+++ 1029. Two City Scheduling.cpp
@@ -25,3 +25,31 @@
 
 
 
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) 
+    {
+        int sum = 0;
+        vector<int> v;
+        
+        for(int i = 0; i < costs.size(); i++)
+        {
+            // send all the person to city A
+            sum += costs[i][0];
+            // find the diifference b/w costs of city A or city B
+            v.push_back(costs[i][1] - costs[i][0]);   
+        }
+        sort(v.begin() , v.end());
+        
+        for(int i = 0; i < costs.size()/2; i++)
+        {
+            sum += v[i];
+        }
+        
+        return sum;
+    }
+};
+
+
+
+
