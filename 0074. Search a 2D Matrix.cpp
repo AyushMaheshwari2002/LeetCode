10433458399f1e331c6c09a3f141c892a9ffdaea
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-21 17:52:01 +0530

    Update 0074. Search a 2D Matrix.cpp

--- 0074. Search a 2D Matrix.cpp
+++ 0074. Search a 2D Matrix.cpp
@@ -13,4 +13,52 @@
 
 
 
+// BINARY SEARCH
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int i = 0;                              // placed at first row of the matrix
+        int j = matrix[0].size() - 1;           // placed at last column of the matrix
+        
+        while(i < matrix.size() && j >= 0)          // check for out of bound
+        {
+            if(matrix[i][j] == target)
+                return true;
+            
+            else if(matrix[i][j] > target)
+                j--;
+            
+            else if(matrix[i][j] < target)
+                i++;
+        }
+        
+        return false;
+    }
+};
+
+
+
+
+/* NAIVE APPROACH
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)
+    {
+        for(int i = 0; i < matrix.size(); i++)
+        {
+            for(int j = 0; j < matrix[0].size(); j++)
+            {
+                if(matrix[i][j] == target)
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+};
+*/
+
+
+
 
