7bd19583785e92d04d177746c4d51a70a2904e17
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-21 17:48:58 +0530

    Update 0674. Longest Continuous Increasing Subsequence.cpp

--- 0674. Longest Continuous Increasing Subsequence.cpp
+++ 0674. Longest Continuous Increasing Subsequence.cpp
@@ -18,3 +18,30 @@
 
 
 
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) 
+    {    
+        if(nums.size() == 0)
+            return 0;
+        if(nums.size() == 1)
+            return 1;
+        
+        int res = 0, count = 1;
+        
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i-1] < nums[i])
+                count++;
+            else
+                count = 1;
+            
+            res = max(count, res);
+        }
+        
+        return res;
+    }
+};
+
+
+
