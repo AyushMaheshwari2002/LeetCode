a435dbf8954309fd8ca388944864e0c84d761366
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-28 23:38:27 +0530

    Update 2063. Vowels of All Substrings.cpp

--- 2063. Vowels of All Substrings.cpp
+++ 2063. Vowels of All Substrings.cpp
@@ -29,3 +29,25 @@
 
 
 
+
+class Solution {
+public:
+    long long countVowels(string word) 
+    {
+        long long ans = 0;
+
+        for(int i = 0 ; i < word.size() ; i++)
+        {
+            if(word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u')
+            {
+                ans += (word.size() - i) * (i + 1);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+
