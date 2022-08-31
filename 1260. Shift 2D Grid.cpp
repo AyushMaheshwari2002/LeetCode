7fd5ddbb78fd499a2955b7019bea93575db25416
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-08-31 23:46:51 +0530

    Update 1260. Shift 2D Grid.cpp

--- 1260. Shift 2D Grid.cpp
+++ 1260. Shift 2D Grid.cpp
@@ -20,3 +20,48 @@
 
 
 
+
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k)
+    {
+        vector<int> v;
+        
+        int row = grid.size();
+        int col = grid[0].size();
+    
+        // push all the elements of 2D matrix into vector
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                v.push_back(grid[i][j]);  
+            }
+        }
+        
+        int idx = v.size()-1;
+        // this will pick the last element from vector and put it in the beginning and remove from last
+        while(k--)
+        {
+            v.insert(v.begin() , v[idx]);
+        
+        }
+        
+        // convert 1D vector into 2D vector
+        int p = 0;
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < col; j++)
+            {
+                grid[i][j] = v[p];
+                p++;
+            }
+        }
+        
+        return grid;
+    }
+};
+
+
+
+
