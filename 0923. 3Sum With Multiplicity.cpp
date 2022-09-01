645a5c595c0f76ae644d2555e9838989f0641c6a
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-01 22:56:44 +0530

    Update 0923. 3Sum With Multiplicity.cpp

--- 0923. 3Sum With Multiplicity.cpp
+++ 0923. 3Sum With Multiplicity.cpp
@@ -28,3 +28,31 @@
 
 
 
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) 
+    {
+        long res = 0, mod = 1000000007;
+        
+        for(int i = 0; i < arr.size(); i++)
+        {
+            vector<int> v(101 , 0);
+            
+            for(int j = i+1; j < arr.size(); j++) 
+            {
+                int k = target - arr[i] - arr[j];
+                
+                if(k >= 0 && k <= 100 && v[k] > 0)
+                {
+                    res += v[k];
+                    res %= mod;
+                }
+                v[arr[j]]++;
+            }
+        }
+        return res;
+    }
+};
+
+
+
