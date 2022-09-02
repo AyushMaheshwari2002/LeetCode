ec8d98bc03be61684718d73992c1317d8035577f
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-02 23:11:11 +0530

    Update 0441. Arranging Coins.cpp

--- 0441. Arranging Coins.cpp
+++ 0441. Arranging Coins.cpp
@@ -15,3 +15,28 @@
 
 
 
+// ITERATIVE METHOD
+class Solution {
+public:
+    int arrangeCoins(int n)
+    {
+        int count = 0;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            if((n - i) < 0)
+            {
+                break;
+            }
+            
+            count++;
+            n = n - i;
+        }
+        
+        return count;
+    }
+};
+
+
+
+
