6ca85d88db87224c244457b524d4a86aa62dfc86
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-03 23:06:22 +0530

    Update 1122. Relative Sort Array.cpp

--- 1122. Relative Sort Array.cpp
+++ 1122. Relative Sort Array.cpp
@@ -13,3 +13,39 @@
 
 
 
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) 
+    {
+        vector<int> v;
+        
+        for(int i = 0; i < arr2.size(); i++)
+        {
+            for(int j = 0; j < arr1.size(); j++)
+            {
+                if(arr1[j] == arr2[i])
+                {
+                    v.push_back(arr1[j]);
+                    arr1[j] = -1;
+                }
+            }
+        }
+            
+        sort(arr1.begin(), arr1.end());
+            
+        // push elements which is remaining in arr1
+        for(int i = 0; i < arr1.size(); i++)
+        {
+            if(arr1[i] != -1)
+            {
+                v.push_back(arr1[i]);
+            }
+        }
+        
+        return v;
+    }
+};
+
+
+
+
