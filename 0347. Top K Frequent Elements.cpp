552b188c55209c7c1ef7f53fc031128f2149d9a1
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-25 23:31:48 +0530

    Update 0347. Top K Frequent Elements.cpp

--- 0347. Top K Frequent Elements.cpp
+++ 0347. Top K Frequent Elements.cpp
@@ -11,3 +11,34 @@
 
 
 
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) 
+    {
+        map<int , int> m;
+        
+        for(int i = 0; i < nums.size(); i++)
+            m[nums[i]]++;
+        
+        vector<pair<int , int>> v;
+        for(auto i : m)
+        {
+            // in vector we have first store the frequency then no. b'coz we have to sort the vector on the basis of increasing frequency
+            v.push_back({i.second , i.first});    
+        }
+        
+        sort(v.begin() , v.end());
+        reverse(v.begin() , v.end());         // the element which has largest frequency comes first
+        
+        vector<int> ans(k);
+        for(int i = 0; i < k; i++)
+        {
+            ans[i] = v[i].second;
+        }
+        
+        return ans;
+    }
+};
+
+
+
