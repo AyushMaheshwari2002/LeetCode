6a34be2c3026769f47814f9c3d01e1121af6306d
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-09-27 22:56:50 +0530

    Update 0682. Baseball Game.cpp

--- 0682. Baseball Game.cpp
+++ 0682. Baseball Game.cpp
@@ -49,4 +49,30 @@
 
 
 
+class Solution {
+public:
+    int calPoints(vector<string>& ops) 
+    {
+        vector<int> v;
+        
+        for(int i = 0; i < ops.size(); i++)
+        {
+            if(ops[i] == "+")
+                v.push_back(v[v.size() - 1] + v[v.size() - 2]);
+            
+            else if(ops[i] == "D")
+                v.push_back(2 * v.back());
+            
+            else if(ops[i] == "C")
+                v.pop_back();
+            
+            else
+                v.push_back(stoi(ops[i]));    //// stoi(converts an string to int) 
+        }
+        
+        return accumulate(v.begin(),v.end(),0);   // sum of vector elements.
+    }
+};
+
+
 
