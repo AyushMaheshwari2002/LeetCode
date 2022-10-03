a8103b619d30abad8bf093e9d1afc8c40ae916ee
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-03 23:05:44 +0530

    Update 1232. Check If It Is a Straight Line.cpp

--- 1232. Check If It Is a Straight Line.cpp
+++ 1232. Check If It Is a Straight Line.cpp
@@ -12,3 +12,25 @@
 
 
 
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& coordinates)
+    {
+        int xdiff = coordinates[1][0] - coordinates[0][0];          // (x2 - x1)
+        int ydiff = coordinates[1][1] - coordinates[0][1];          // (y2 - y1)
+        
+        for(int i = 2; i < coordinates.size(); i++) 
+        {
+            int curr_xdiff = coordinates[i][0] - coordinates[0][0];       // (x3 - x2)
+            int curr_ydiff = coordinates[i][1] - coordinates[0][1];       // (y3 - y2)
+            
+            if(curr_xdiff * ydiff != curr_ydiff * xdiff) 
+                return false; 
+        }
+        
+        return true;
+    }
+};
+
+
+
