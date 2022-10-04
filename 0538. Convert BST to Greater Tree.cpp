4d20c37f2bb4d593f6f7983cffc4bf1fc5620c13
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-04 23:25:43 +0530

    Update 0538. Convert BST to Greater Tree.cpp

--- 0538. Convert BST to Greater Tree.cpp
+++ 0538. Convert BST to Greater Tree.cpp
@@ -17,3 +17,24 @@
 
 
 
+class Solution {
+    int sum = 0;
+public:
+    TreeNode* convertBST(TreeNode* root) 
+    {
+        if(root == NULL)
+            return root;
+        
+        convertBST(root -> right);
+        
+        sum += root -> val;
+        root -> val = sum;
+        
+        convertBST(root -> left);
+        
+        return root;
+    }
+};
+
+
+
