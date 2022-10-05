a315acaf7af9775aeb173a738da35bf50dfc713f
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-05 23:42:35 +0530

    Update 0543. Diameter of Binary Tree.cpp

--- 0543. Diameter of Binary Tree.cpp
+++ 0543. Diameter of Binary Tree.cpp
@@ -14,3 +14,30 @@
 
 
 
+class Solution {
+public:
+    
+    int diameter = 0;
+    int diameterOfBinaryTree(TreeNode* root) 
+    {
+        int data = height(root);
+        
+        return diameter-1;
+    }
+    
+    int height(TreeNode* root)
+    {
+        if(root == NULL)
+            return 0;
+        
+        int lh = height(root -> left);
+        int rh = height(root -> right);
+        
+        diameter = max(diameter , 1+lh+rh);
+        
+        return 1 + max(lh , rh);
+    }
+};
+
+
+
