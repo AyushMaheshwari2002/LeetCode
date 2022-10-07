a682d3b32068a653930a861021a5025cfc60fbce
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-07 23:36:59 +0530

    Update 0695. Max Area of Island.cpp

--- 0695. Max Area of Island.cpp
+++ 0695. Max Area of Island.cpp
@@ -4,8 +4,8 @@
     Return the maximum area of an island in grid. If there is no island, return 0.
 
           Example 1:
-                Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0]
-                ,[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+                Input: grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],
+                [0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
                 Output: 6
                 Explanation: The answer is not 11, because the island must be connected 4-directionally.
 
@@ -13,6 +13,40 @@
                 Input: grid = [[0,0,0,0,0,0,0,0]]
                 Output: 0
 */
+
+
+
+class Solution {
+public:
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid)
+    {
+        int area = 0;
+        for(int i = 0; i < grid.size(); i++)
+        {
+            for(int j = 0; j < grid[0].size(); j++)
+            {
+                if(grid[i][j] == 1)
+                { 
+                    area = max(area, dfs(grid, i, j));
+                }
+            }
+        }
+        return area;
+    }
+    
+    int dfs(vector<vector<int>>& grid, int i, int j)
+    {
+        if(i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size() && grid[i][j] == 1)
+        {
+            // mark 0 for visited values
+            grid[i][j] = 0;
+            return 1 + dfs(grid, i+1, j) + dfs(grid, i-1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1);
+        }
+        return 0;
+    }
+};
+
 
 
 
