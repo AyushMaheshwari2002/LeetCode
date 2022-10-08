cdae5174ced09d4fbc51ee8a5628dd5a243ff738
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-08 23:49:19 +0530

    Update 0705. Design HashSet.cpp

--- 0705. Design HashSet.cpp
+++ 0705. Design HashSet.cpp
@@ -27,4 +27,27 @@
 
 
 
+class MyHashSet {
+public:
+    
+    vector<bool> v;
+    MyHashSet() {
+        v.resize(1e6+1, false);
+    }
+    
+    void add(int key) {
+        v[key] = true;
+    }
+    
+    void remove(int key) {
+        v[key] = false;
+    }
+    
+    // if key is already present then return true else false
+    bool contains(int key) {
+        return v[key];
+    }
+};
+
+
 
