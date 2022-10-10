6bf2607a5fc4c8859050a46a70ed5cf62822cd52
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-10 23:40:58 +0530

    Update 0092. Reverse Linked List II.cpp

--- 0092. Reverse Linked List II.cpp
+++ 0092. Reverse Linked List II.cpp
@@ -11,3 +11,67 @@
 */
 
 
+
+class Solution {
+public:
+    
+    ListNode* reverse(ListNode* head) 
+    {    
+        ListNode* previous = NULL;
+        ListNode* current = head;
+        ListNode* next;
+        
+        while(current != NULL)
+        {
+            next = current -> next;
+            current -> next = previous;
+            
+            previous = current;
+            current = next;
+        }
+        
+        return previous;
+    }
+    
+    ListNode* reverseBetween(ListNode* head, int left, int right) 
+    {
+        ListNode* curr = head, *prev = NULL;
+        int count = 1;
+        
+        while(count != left) {
+            prev = curr;
+            curr = curr -> next;
+            count++;
+        }
+        
+        // this is the node where we have to start reversing the node
+        ListNode* start = curr;
+        
+        while(count != right) {
+            curr = curr -> next;
+            count++;
+        }
+        
+        ListNode* connect = curr -> next;
+        curr -> next = NULL;
+        
+        ListNode* newHead = reverse(start);
+        
+        if(prev != NULL)
+            prev -> next = newHead;
+        
+        curr = newHead;
+        while(curr -> next != NULL)
+            curr = curr -> next;
+        
+        curr -> next = connect;
+        
+        if(left == 1)
+            return newHead;
+        return head;
+    }
+};
+
+
+
+
