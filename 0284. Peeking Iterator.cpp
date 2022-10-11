6173b4bc1465f53ec6cdd32082c43264ecccb09d
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-11 23:04:09 +0530

    Update 0284. Peeking Iterator.cpp

--- 0284. Peeking Iterator.cpp
+++ 0284. Peeking Iterator.cpp
@@ -27,4 +27,68 @@
 
 
 
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        Iterator it = *this;
+        return it.next();
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+        // here we have call parent class method to child class using iterator
+	    return Iterator :: next();
+	}
+	
+	bool hasNext() const {
+	    return Iterator :: hasNext();
+	}
+};
+
+
+
+/*
+class PeekingIterator : public Iterator {
+public:
+    
+    vector<int> v;
+    int idx = 0;
+    
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    v = nums; // create a copy of nums vector
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        // return element without incrementing the pointer
+        return v[idx];
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+        // return element and increment the pointer
+	    return v[idx++];
+	}
+	
+	bool hasNext() const {
+	    if(idx < v.size())
+            return true;
+        return false;
+	}
+};
+*/
+
 
