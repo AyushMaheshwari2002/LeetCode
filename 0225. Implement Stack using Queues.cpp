bf344229252426029a678336f623ef6e1b7adea6
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-21 21:30:35 +0530

    Update 0225. Implement Stack using Queues.cpp

--- 0225. Implement Stack using Queues.cpp
+++ 0225. Implement Stack using Queues.cpp
@@ -31,4 +31,36 @@
 
 
 
+class MyStack {
+public:
+    queue<int>  q;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i = 1; i<q.size(); ++i)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int a = q.front();
+        q.pop();
+        return a;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+
 
