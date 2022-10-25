526d4b496b397127c1f6b76bf0d2e7981bfe4153
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-25 23:22:15 +0530

    Update 0232. Implement Queue using Stacks.cpp

--- 0232. Implement Queue using Stacks.cpp
+++ 0232. Implement Queue using Stacks.cpp
@@ -1,5 +1,4 @@
-/*  Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, 
-    and empty).
+/*  Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
 
     Implement the MyQueue class:
          * void push(int x) Pushes element x to the back of the queue.
@@ -29,6 +28,58 @@
                       myQueue.pop(); // return 1, queue is [2]
                       myQueue.empty(); // return false
 */
+
+
+
+class MyQueue { 
+public:
+    
+    stack<int>s1 , s2;
+    
+    MyQueue() {}
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop()
+    {
+        while(!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        int ans = s2.top();
+        s2.pop();
+        while(!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return ans;
+    }
+    
+    int peek()
+    {
+        while(!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+        int ans = s2.top();
+        while(!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return ans;
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
+
 
 
 
