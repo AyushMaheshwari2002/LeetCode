0a04cc501d353775bb901f88934fb01c92cdf657
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-10-29 23:34:56 +0530

    Update 0073. Set Matrix Zeroes.cpp

--- 0073. Set Matrix Zeroes.cpp
+++ 0073. Set Matrix Zeroes.cpp
@@ -12,4 +12,32 @@
 
 
 
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix)
+    {
+        vector<pair<int,int>> v;
+        for(int i = 0; i < matrix.size(); i++)
+        {
+            for(int j = 0; j < matrix[0].size(); j++)
+            {
+                if(matrix[i][j] == 0)
+                    v.push_back(make_pair(i,j));
+            }
+        }
+        
+        for(int i = 0; i < v.size(); i++)
+        {
+            int row = v[i].first, col = v[i].second;
+            
+            for(int i = 0; i < matrix[0].size(); i++)
+                matrix[row][i] = 0;
+            
+            for(int j = 0; j < matrix.size(); j++)
+                matrix[j][col] = 0;
+        }
+    }
+};
+
+
 
