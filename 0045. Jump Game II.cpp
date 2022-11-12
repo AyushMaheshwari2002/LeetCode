e3eb64eb5c7fd18812f1a32df43aff0d42b2ef1d
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-12 23:12:38 +0530

    Update 0045. Jump Game II.cpp

--- 0045. Jump Game II.cpp
+++ 0045. Jump Game II.cpp
@@ -16,3 +16,28 @@
 
 
 
+class Solution {
+public:
+    int jump(vector<int>& nums)
+    {
+        int max_reach = 0;
+        int jumps = 0;
+        int current = 0;
+        
+        for(int i = 0; i < nums.size()-1; i++)
+        {
+            max_reach = max(max_reach , i + nums[i]);
+            
+            if(i == current)
+            {
+                current = max_reach;
+                jumps++;
+            }
+        }
+        
+        return jumps;
+    }
+};
+
+
+
