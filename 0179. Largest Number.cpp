2a35d8c02e2d42665466c3dda60faeb4d4cc306d
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-17 23:29:36 +0530

    Update 0179. Largest Number.cpp

--- 0179. Largest Number.cpp
+++ 0179. Largest Number.cpp
@@ -12,4 +12,46 @@
 
 
 
+class Solution {
+public:
+    
+    //here we create a custom sort function.
+    static bool cmp(string a , string b)
+    {
+        return (a+b) > (b+a);
+    }
+    
+    string largestNumber(vector<int>& nums) 
+    {
+        //written to handle the corner case like [0,0,0] in these cases of all zeros we need to return 0 only. 
+        int x = 0 ; 
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == 0)
+                x++;
+        }
+        if(x == nums.size())
+            return "0";
+        
+        vector<string> v;
+        
+        for(auto i : nums)
+            v.push_back(to_string(i));
+        
+        sort(v.begin() , v.end() , cmp);
+        
+        string s = "";
+        
+        for(auto i : v)
+        {
+            s += i;
+        }
+		
+        return s;
+        
+    }
+};
+
+
 
