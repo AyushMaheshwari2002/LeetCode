a0620bf394c9d932a97bd68efae93c1b543bc8d8
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-18 22:57:47 +0530

    Update 0762. Prime Number of Set Bits in Binary Representation.cpp

--- 0762. Prime Number of Set Bits in Binary Representation.cpp
+++ 0762. Prime Number of Set Bits in Binary Representation.cpp
@@ -28,3 +28,37 @@
 
 
 
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right)
+    {
+        int count = 0;
+        for(int i = left; i <= right; i++)
+        {
+            if(checkPrime(countSetBits(i))) 
+                count++;
+        }
+        return count;
+    }
+    
+    int countSetBits(int n)
+    {
+        int count = 0;
+        
+        while(n != 0)
+        {
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+    }
+    
+    bool checkPrime(int x)
+    {
+        return(x == 2 || x == 3 || x == 5 || x == 7 || x == 11 || x == 13 || x == 17 || x == 19);
+    }
+};
+
+
+
+
