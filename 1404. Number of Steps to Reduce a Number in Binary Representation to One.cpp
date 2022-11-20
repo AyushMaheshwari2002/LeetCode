90d8804ded36f7368f2323ffdf1ec2c3430faf09
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-20 23:21:01 +0530

    Update 1404. Number of Steps to Reduce a Number in Binary Representation to One.cpp

--- 1404. Number of Steps to Reduce a Number in Binary Representation to One.cpp
+++ 1404. Number of Steps to Reduce a Number in Binary Representation to One.cpp
@@ -27,4 +27,25 @@
 
 
 
+class Solution {
+public:
+    int numSteps(string s) 
+    {
+        int res = 0, carry = 0;
+        
+        for(int i = s.size()-1; i > 0; i--)
+        {
+            res++;
+            
+            if((s[i] - '0') + carry == 1) 
+            {
+                carry = 1;
+                res++;
+            }
+        }
+        return res + carry;
+    }
+};
+
+
 
