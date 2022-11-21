ab388fcd412d372722faf9134303b09cc6da7f23
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-21 23:39:47 +0530

    Update 1980. Find Unique Binary String.cpp

--- 1980. Find Unique Binary String.cpp
+++ 1980. Find Unique Binary String.cpp
@@ -19,3 +19,20 @@
 
 
 
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) 
+    {
+        string ans = "";
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            ans += nums[i][i] == '0' ? '1' : '0';
+        }
+        
+        return ans;
+    }
+};
+
+
+
