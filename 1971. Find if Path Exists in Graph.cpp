ec16a8af05c84837c6c9fe482e7d1765461c1e95
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-23 23:27:34 +0530

    Update 1971. Find if Path Exists in Graph.cpp

--- 1971. Find if Path Exists in Graph.cpp
+++ 1971. Find if Path Exists in Graph.cpp
@@ -19,3 +19,45 @@
 
 
 
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) 
+    {
+        vector<vector<int>> adjList(n);
+        
+        for(int i = 0; i < edges.size(); i++)
+        {
+            adjList[edges[i][0]].push_back(edges[i][1]);
+            adjList[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        vector<int> visited(n);
+        
+        queue<int> q;
+        q.push(source);
+        visited[source] = 1;
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            
+            if(node == destination) 
+                return true;
+            
+            for(auto it : adjList[node])
+            {
+                if(!visited[it])
+                {
+                    visited[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        
+        return false;
+    }
+};
+
+
+
