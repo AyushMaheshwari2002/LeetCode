659edc088f0ca84974c663479386918b4aecfa17
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-25 22:40:20 +0530

    Update 0205. Isomorphic Strings.cpp

--- 0205. Isomorphic Strings.cpp
+++ 0205. Isomorphic Strings.cpp
@@ -18,4 +18,36 @@
 
 
 
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) 
+    {    
+        if(s.size() != t.size())
+            return false;
+        
+        map<char,char> m;
+        set<char> st;
+        
+        for(int i = 0; i < s.size(); i++)
+        {    
+            if(m.find(s[i]) != m.end())
+            {
+                if(m[s[i]] != t[i])
+                    return false;
+            } 
+            else 
+            {
+                if(st.count(t[i]) > 0)
+                    return false;
+                
+                st.insert(t[i]);
+                m[s[i]] = t[i];
+            }
+        }
+        
+        return true;
+    }
+};
+
+
 
