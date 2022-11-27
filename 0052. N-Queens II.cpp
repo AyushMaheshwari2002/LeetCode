782108d928b8bc19a6c38fe6183f54a6392d4a24
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-27 22:50:01 +0530

    Update 0052. N-Queens II.cpp

--- 0052. N-Queens II.cpp
+++ 0052. N-Queens II.cpp
@@ -13,3 +13,74 @@
 
 
 
+class Solution {
+public:
+    
+    bool isSafe(int row, int col, vector<string> board)
+    {
+        int duprow = row;
+        int dupcol = col;
+        
+        while(row >= 0 && col >= 0)
+        {
+            if(board[row][col] == 'Q')
+                return false;
+            --row;
+            --col;
+        }
+        
+        col = dupcol;
+        row = duprow;
+        while(col >= 0)
+        {
+            if(board[row][col] == 'Q')
+                return false;
+            --col;
+        }
+        
+        row = duprow;
+        col = dupcol;
+        while(row < board.size() && col >= 0)
+        {
+            if(board[row][col] == 'Q')
+                return false;
+            ++row;
+            --col;
+        }
+        return true;
+    }
+    
+    int NQueens(int col, vector<string>& board)
+    {
+        if(col == board.size())
+            return 1;
+        
+        int cnt = 0;
+        
+        for(int row = 0; row < board.size(); ++row)
+        {
+            if(isSafe(row,col,board)){
+                board[row][col] = 'Q';
+                cnt += NQueens(col + 1, board);
+                board[row][col] = '.';
+            }
+        }
+        return cnt;
+    }
+    
+    
+    int totalNQueens(int n) {
+        vector<string> board(n);
+        string s(n,'.');
+        
+        for(int i = 0; i<n; ++i)
+        {
+            board[i] = s;
+        }
+        
+        return NQueens(0,board);
+    }
+};
+
+
+
