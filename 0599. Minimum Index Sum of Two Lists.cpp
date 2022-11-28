e039f033aa7f545e382945625582e30145010fc1
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-28 23:40:50 +0530

    Update 0599. Minimum Index Sum of Two Lists.cpp

--- 0599. Minimum Index Sum of Two Lists.cpp
+++ 0599. Minimum Index Sum of Two Lists.cpp
@@ -26,3 +26,35 @@
 
 
 
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2)
+    {
+        vector<string> v;
+        int min_index = INT_MAX;
+        
+        for(int i = 0; i < list1.size(); i++)
+        {
+            for(int j = 0; j < list2.size(); j++)
+            {
+                if(list1[i] == list2[j] && i+j == min_index)
+                {
+                    v.push_back(list1[i]);
+                    min_index = i + j;
+                }
+                
+                else if(list1[i] == list2[j] && i+j < min_index)
+                {
+                    v.clear();
+                    v.push_back(list1[i]);
+                    min_index = i + j;
+                }
+            }
+        }
+        
+        return v;
+    }
+};
+
+
+
