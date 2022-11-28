c727c8fde24a37ef8fa7cb2046416820060d450c
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-11-28 23:43:18 +0530

    Update and rename 739. Daily Temperatures.cpp to 0739. Daily Temperatures.cpp

--- 0739. Daily Temperatures.cpp
+++ 0739. Daily Temperatures.cpp
@@ -16,3 +16,27 @@
 
 
 
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures)
+    {
+        int n = temperatures.size();
+        vector<int> v(n , 0);
+        stack<int> s;
+        
+        for(int i = 0; i < n; i++)
+        {
+            while(!s.empty() && temperatures[s.top()] < temperatures[i])
+            {
+                v[s.top()] = i - s.top();
+                s.pop();
+            }
+            s.push(i);
+        }
+        
+        return v;
+    }
+};
+
+
+
