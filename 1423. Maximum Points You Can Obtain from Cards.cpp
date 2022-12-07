9c9ea5c6039ebd2c1ccb95b251b0a4d84279f344
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-12-07 23:40:37 +0530

    Update 1423. Maximum Points You Can Obtain from Cards.cpp

--- 1423. Maximum Points You Can Obtain from Cards.cpp
+++ 1423. Maximum Points You Can Obtain from Cards.cpp
@@ -22,4 +22,30 @@
 
 
 
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) 
+    {
+        int sum = 0;
+        for(int i = 0; i<k; ++i)
+            sum += cardPoints[i];
+        
+        if(cardPoints.size() == k)
+            return sum;
+        
+        int j = cardPoints.size()-1;
+        int i = k-1, currsum =sum;
+        
+        while(i >= 0)
+        {
+            currsum -= cardPoints[i--];
+            currsum += cardPoints[j--];
+            sum = max(sum,currsum);
+        }
+        
+        return sum;
+    }
+};
+
+
 
