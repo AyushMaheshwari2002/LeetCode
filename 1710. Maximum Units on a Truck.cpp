63ba284a4feeaf1f998b4adc6f3724c60523a8f1
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-12-09 23:39:33 +0530

    Update 1710. Maximum Units on a Truck.cpp

--- 1710. Maximum Units on a Truck.cpp
+++ 1710. Maximum Units on a Truck.cpp
@@ -22,3 +22,26 @@
 
 
 
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        
+        int ans = 0;
+        
+        sort(boxTypes.begin(),boxTypes.end(),[&](vector<int> &a, vector<int> &b)
+             {
+                 return a[1] > b[1];
+             });
+            
+        for(auto box : boxTypes)
+        {
+            ans += min(box[0],truckSize) * box[1];
+            truckSize -= min(truckSize, box[0]);
+        }
+        
+        return ans;
+    }
+};
+
+
+
