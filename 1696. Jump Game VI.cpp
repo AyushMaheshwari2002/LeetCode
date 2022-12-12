b33602a6367c55fa387685b03f5e365d2184da68
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-12-12 23:31:04 +0530

    Update 1696. Jump Game VI.cpp

--- 1696. Jump Game VI.cpp
+++ 1696. Jump Game VI.cpp
@@ -21,4 +21,28 @@
 
 
 
+class Solution {
+public:
+    
+    int maxResult(vector<int>& nums, int k) 
+    {
+        
+        deque<int> d{0};
+        
+        for (int i = 1; i < nums.size(); ++i) 
+        {
+            if (d.front() + k < i)
+                d.pop_front();        
+            nums[i] += nums[d.front()];
+            
+            while (!d.empty() && nums[d.back()] <= nums[i])
+                d.pop_back();
+            
+            d.push_back(i);
+        }
+        return nums.back();
+    }
+};
+
+
 
