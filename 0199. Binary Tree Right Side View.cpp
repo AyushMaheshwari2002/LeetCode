de7e244dbd5a33ea34ea7a5a11f9fe954085385b
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-12-13 23:37:14 +0530

    Update 0199. Binary Tree Right Side View.cpp

--- 0199. Binary Tree Right Side View.cpp
+++ 0199. Binary Tree Right Side View.cpp
@@ -15,3 +15,30 @@
 
 
 
+class Solution {
+public:
+
+    void solve(TreeNode* root, int level, vector<int>&res)
+    {
+        if(root == NULL)
+            return;
+
+        if(res.size() == level) { 
+            res.push_back(root->val);
+        }
+        
+        solve(root->right, level+1, res);
+        solve(root->left, level+1, res);   
+    }
+
+    vector<int> rightSideView(TreeNode* root) 
+    {
+        vector<int> res;
+        solve(root, 0, res);
+        
+        return res;
+    }
+};
+
+
+
