5824005807b5ef7a54eb4fe85985852354e440c1
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-12-17 23:01:19 +0530

    Update 0315. Count of Smaller Numbers After Self.cpp

--- 0315. Count of Smaller Numbers After Self.cpp
+++ 0315. Count of Smaller Numbers After Self.cpp
@@ -19,3 +19,79 @@
 */
 
 
+
+class Solution {
+public:
+    
+    void merge(vector<int> &count, vector<pair<int, int>> &v, int left, int mid, int right)
+    {
+        // take a temporary array which store value in decreasing order
+        vector<pair<int,int>> temp(right - left + 1);
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+        
+        while(i <= mid && j <= right)
+        {   
+            if(v[i].first <= v[j].first)
+            {
+                temp[k++] = v[j++];
+            }
+            
+            else
+            {   // case : 5 > 2 then increase the count vector
+                count[v[i].second] += right-j+1;
+                temp[k++] = v[i++];         // here we have increase the count and store the values in temp vector in decreasing order
+            }
+        }
+        
+        while(i <= mid)
+        {
+            temp[k++] = v[i++];
+        }
+        
+        while(j <= right)
+        {
+            temp[k++] = v[j++];
+        }
+        
+        for(int i = left; i <= right; i++)
+        {
+            v[i] = temp[i-left]; 
+        }    
+    }
+    
+    void mergeSort(vector<int> &count, vector<pair<int,int>> &v, int left, int right)
+    {    
+        if(left < right)
+        {
+            int mid = left + (right-left)/2;
+            
+            mergeSort(count, v, left, mid);
+            mergeSort(count, v, mid+1, right);
+            
+            merge(count, v, left, mid, right);
+        }  
+    }
+    
+    vector<int> countSmaller(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<pair<int,int>> v(n);
+        
+        // we have store the value and the index in vector
+        for(int i = 0; i < n; i++)
+        {
+            v[i] = {nums[i] , i};
+        }
+        
+        // this vector store the answer which we have to return
+        vector<int> count(n, 0);
+        mergeSort(count, v, 0, n-1);
+        
+        return count;
+    }
+};
+
+
+
