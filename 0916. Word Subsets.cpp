76631ae74aed1f0166a0c2991a2b3d1d561d2fba
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-12-19 23:38:38 +0530

    Update 0916. Word Subsets.cpp

--- 0916. Word Subsets.cpp
+++ 0916. Word Subsets.cpp
@@ -15,3 +15,47 @@
 
 
 
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        
+        vector<int> freq(26,0);
+        
+        for(auto itr : words2)
+        {
+            vector<int> temp(26,0);
+            for(auto x : itr)
+            {
+                ++temp[x - 'a'];
+                freq[x - 'a'] = max(temp[x - 'a'],freq[x - 'a']);
+            }
+        }
+        
+        vector<string> ans;
+        
+        for(auto itr : words1)
+        {
+            vector<int> temp(26,0);
+            for(auto x : itr)
+                ++temp[x - 'a'];
+            
+            bool flag = true;
+            
+            for(int i = 0; i<26; ++i)
+            {
+                if(freq[i] > temp[i])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+                ans.push_back(itr);
+        }
+        return ans;
+    }
+};
+
+
+
+
