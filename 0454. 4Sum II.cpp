e77e744fc3a06e32a0c9a38bcea865e4c43268ec
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-12-20 23:36:43 +0530

    Update 0454. 4Sum II.cpp

--- 0454. 4Sum II.cpp
+++ 0454. 4Sum II.cpp
@@ -18,3 +18,89 @@
 
 
 
+class Solution {
+public: 
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4)
+    {
+        int count = 0;
+        unordered_map<int,int> m;
+        
+        for(int i : nums1)
+        { 
+            for(int j : nums2)
+            {
+                m[i+j]++;
+            }
+        }
+            
+        for(int k : nums3)
+        { 
+            for(int l : nums4)
+            { 
+                count += m[-(k+l)];
+            }
+        }
+        
+        return count;
+    }
+};
+
+
+
+/* TC : O(N^3)
+class Solution {
+public: 
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4)
+    {
+        int count = 0;
+        unordered_map<int,int> m;
+        
+        for(int i : nums1)
+        { 
+            m[i]++;
+            
+            for(int j : nums2)
+            { 
+                for(int k : nums3)
+                { 
+                    for(int l : nums4)
+                    { 
+                        count += m[-(j+k+l)];
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
+*/
+
+
+
+/* TC : O(N^4)
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4)
+    {
+        int count = 0;
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            for(int j = 0; j < nums2.size(); j++)
+            {
+                for(int k = 0; k < nums3.size(); k++)
+                {
+                    for(int l = 0; l < nums4.size(); l++)
+                    {
+                        if(nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0)
+                            count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
+*/
+
+
+
