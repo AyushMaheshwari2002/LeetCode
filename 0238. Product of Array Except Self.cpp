e9364ec1816ed99dba6421011e99bb48332271c3
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2022-12-30 23:00:18 +0530

    Update 0238. Product of Array Except Self.cpp

--- 0238. Product of Array Except Self.cpp
+++ 0238. Product of Array Except Self.cpp
@@ -13,3 +13,62 @@
 
 
 
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<int> v(n);
+        
+        int rp = 1;          // rp = running_product
+        
+        // left pass
+        for(int i = 0; i < n; i++)
+        {
+            v[i] = rp;
+            rp = nums[i] * rp;
+        }
+        
+        // right pass
+        rp = 1;
+        for(int i = n-1; i >= 0; i--)
+        {
+            v[i] = v[i] * rp;
+            rp = nums[i] * rp;
+        }
+        
+        return v;
+    }
+};
+
+
+
+
+/*
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<int> v(n, 1);
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(i == j)
+                {
+                    continue;
+                }
+                
+                v[i] = v[i] * nums[j]; 
+            }
+        }
+
+        return v;
+    }
+};
+*/
+
+
+
