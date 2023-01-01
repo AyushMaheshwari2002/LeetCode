6e8727293dab2eb1485c6c27bbed4aaf5a7eff8c
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-01-01 20:53:20 +0530

    Update 0804. Unique Morse Code Words.cpp

--- 0804. Unique Morse Code Words.cpp
+++ 0804. Unique Morse Code Words.cpp
@@ -28,3 +28,29 @@
 
 
 
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words)
+    {
+        vector<string> morse_code = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        
+        unordered_set<string> s;
+        
+        for(int i = 0; i < words.size(); i++)
+        {
+            string str;
+            
+            for(int j = 0; j < words[i].size(); j++)
+            {
+                str += morse_code[words[i][j] - 'a'];
+            }
+            s.insert(str);
+        }
+        
+        return s.size();
+    }
+};
+
+
+
+
