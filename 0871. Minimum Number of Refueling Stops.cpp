f0a569bfdd2deb1968932eadc556386517c237d7
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-01-02 22:11:54 +0530

    Update 0871. Minimum Number of Refueling Stops.cpp

--- 0871. Minimum Number of Refueling Stops.cpp
+++ 0871. Minimum Number of Refueling Stops.cpp
@@ -28,3 +28,40 @@
 */
 
 
+
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>> &stations)
+    {
+        int max_reach = startFuel;  
+
+        priority_queue<int> pq;
+
+        int count = 0;  
+        int idx = 0;                // index of next station
+
+        while(max_reach < target)
+        {
+            while(idx < stations.size() && stations[idx][0] <= max_reach)
+            {
+                pq.push(stations[idx][1]);
+                idx++;
+            }
+
+            if(pq.empty())
+            {
+                return -1;
+            }
+
+            // we can refuel
+            max_reach += pq.top();
+            pq.pop();
+            count++;
+
+        }
+        return count;
+    }
+};
+
+
+
