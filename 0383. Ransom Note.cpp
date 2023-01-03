59eb0f2599ada6e6232d24fec75492b2424ba36c
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-01-03 22:34:50 +0530

    Update 0383. Ransom Note.cpp

--- 0383. Ransom Note.cpp
+++ 0383. Ransom Note.cpp
@@ -15,3 +15,25 @@
 */
 
 
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        unordered_map<char,int> m;
+        
+        for(auto i : magazine)
+            m[i]++;
+        
+        for(auto i : ransomNote)
+        {
+            if(m[i]-- == 0)
+                return false;
+        }
+        
+        return true;
+    }
+};
+
+
+
