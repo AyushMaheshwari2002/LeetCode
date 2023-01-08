3dd25e05a5e01002838c36325c122554b01e900b
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-01-08 22:16:31 +0530

    Update 2022. Convert 1D Array Into 2D Array.cpp

--- 2022. Convert 1D Array Into 2D Array.cpp
+++ 2022. Convert 1D Array Into 2D Array.cpp
@@ -25,3 +25,28 @@
 */
 
 
+
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) 
+    {
+        if(m*n != original.size())
+            return {};
+        
+        vector<vector<int>> v(m, vector<int> (n));
+        int idx = 0;
+        
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                v[i][j] = original[idx++];        
+            }
+        }
+        
+        return v;
+    }
+};
+
+
+
