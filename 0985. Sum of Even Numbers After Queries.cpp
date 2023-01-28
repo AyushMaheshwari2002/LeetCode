a8bd03f30fef3db6e128c1f98220475a72347662
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-01-28 22:45:18 +0530

    Update 0985. Sum of Even Numbers After Queries.cpp

--- 0985. Sum of Even Numbers After Queries.cpp
+++ 0985. Sum of Even Numbers After Queries.cpp
@@ -18,3 +18,39 @@
 
 
 
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        
+        vector<int> ans;        
+        int sum = 0;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] % 2 == 0)
+                sum += nums[i];
+        }
+ 
+        for(int i = 0; i < queries.size(); i++)
+        {
+            int val = queries[i][0];
+            int index = queries[i][1];
+            
+            if(nums[index] % 2 == 0)
+                sum -= nums[index];
+            
+            nums[index] += val;
+            
+            if(nums[index] % 2 == 0)
+                sum += nums[index];
+            
+            ans.push_back(sum);
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
