cacc5b60759598f6aa98d64a1ccea29ff9eded6e
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-02-06 23:40:09 +0530

    Update 2414. Length of the Longest Alphabetical Continuous Substring.cpp

--- 2414. Length of the Longest Alphabetical Continuous Substring.cpp
+++ 2414. Length of the Longest Alphabetical Continuous Substring.cpp
@@ -17,3 +17,28 @@
 
 
 
+class Solution {
+public:
+    int longestContinuousSubstring(string s)
+    {
+        int count= 1, ans = 1;
+        
+        for(int i = 0; i < s.length()-1; i++)
+        {
+            if(s[i] == s[i+1]-1)
+            {
+                count++;
+                ans = max(ans,count);
+            }
+            else 
+            {
+                count = 1;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+
