43c0e19f5e4fee7d39223cd54df45062a494bbee
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-02-10 21:35:50 +0530

    Update 0218. The Skyline Problem.cpp

--- 0218. The Skyline Problem.cpp
+++ 0218. The Skyline Problem.cpp
@@ -24,3 +24,50 @@
 */
 
 
+
+class Solution {
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) 
+    {
+        vector<pair<int, int>> v;
+		
+        // Convert each building into two segments.
+        for(auto b : buildings) {
+            v.push_back({b[0], -b[2]});
+            v.push_back({b[1], b[2]});
+        }
+        
+        sort(v.begin(), v.end());
+        int prev = 0, cur = 0;
+        
+        multiset<int> m;
+        vector<vector<int>> res;
+        
+        m.insert(0);
+        
+        for(auto i : v)
+        {    
+            // If i.second is less than zero, then it means it is left boundary.
+            if (i.second < 0) {
+                m.insert(-i.second);
+            }
+            else { 
+                // else it is right boundary.
+                m.erase(m.find(i.second));
+            } 
+            
+            cur = *m.rbegin();
+            
+            // If current maximum height is not equal to maximum previuous height, it is a key point.
+            if (cur != prev) {
+                res.push_back({i.first, cur});
+                prev = cur;
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+
