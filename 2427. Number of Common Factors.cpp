46ec5936510f61071b17c377e4bb0f9eaefee20d
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-03-16 23:40:45 +0530

    Update 2427. Number of Common Factors.cpp

--- 2427. Number of Common Factors.cpp
+++ 2427. Number of Common Factors.cpp
@@ -13,3 +13,25 @@
 */
 
 
+
+class Solution {
+public:
+    int commonFactors(int a, int b) 
+    {
+        int ans = 0;
+        
+        // common factor will never be increased by the minimum value
+        int n = min(a,b);
+        
+        for(int i = 1; i <= n; i++)
+        {
+            if(a % i == 0 && b % i == 0)
+                ans++;
+        }
+        
+        return ans;
+    }
+};
+
+
+
