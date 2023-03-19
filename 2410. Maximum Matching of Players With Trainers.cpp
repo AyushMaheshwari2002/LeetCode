d9464ff2275e218f782425a186923e3f66ad0e8a
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-03-19 23:36:15 +0530

    Update 2410. Maximum Matching of Players With Trainers.cpp

--- 2410. Maximum Matching of Players With Trainers.cpp
+++ 2410. Maximum Matching of Players With Trainers.cpp
@@ -22,3 +22,35 @@
 */
 
 
+
+// Two Pointer Approach
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) 
+    {
+        int cnt = 0;
+        int i = 0, j = 0;
+        
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+        
+        while(i < players.size() && j < trainers.size())
+        {
+            if(players[i] <= trainers[j])
+            {
+                cnt++;
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+        
+        return cnt;
+    }
+};
+
+
+
+
