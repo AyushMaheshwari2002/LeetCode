a063c8f90454971d22d43ad1c03cfbc1d0978fea
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-03-20 23:38:03 +0530

    Update 1492. The kth Factor of n.cpp

--- 1492. The kth Factor of n.cpp
+++ 1492. The kth Factor of n.cpp
@@ -18,3 +18,37 @@
 */
 
 
+
+class Solution {
+public:
+    int kthFactor(int n, int k) 
+    {
+        vector<int> v;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            if(n % i == 0)
+                v.push_back(i);
+        }
+        
+        if(k > v.size())
+            return -1;
+        
+        return v[k-1];
+        
+        
+        /*
+        for(auto i : v)
+        {
+            k--;
+            if(k == 0)
+                return i;
+        }
+        
+        return -1;
+        */
+    }
+};
+
+
+
