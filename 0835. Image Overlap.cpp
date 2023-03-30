1dc474990ab8d2e34267515a1d6e90df9c2e58de
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-03-30 23:29:44 +0530

    Update 0835. Image Overlap.cpp

--- 0835. Image Overlap.cpp
+++ 0835. Image Overlap.cpp
@@ -20,3 +20,44 @@
 */
 
 
+
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2)
+    {
+        vector<pair<int,int>> a;
+        vector<pair<int,int>> b;
+        
+        for(int i = 0; i < img1.size(); i++)
+        {
+            for(int j = 0; j < img1.size(); j++)
+            {
+                if(img1[i][j] == 1)
+                {
+                    a.push_back({i,j});
+                }
+                if(img2[i][j] == 1)
+                {
+                    b.push_back({i,j});
+                }
+            }
+        }
+        
+        int ans = 0;
+        map<pair<int,int>,int> m;
+        
+        for(auto [i1,j1] : a)
+        {
+            for(auto [i2,j2] : b)
+            {
+                m[{i1-i2,j1-j2}]++;
+                ans = max(ans,m[{i1-i2,j1-j2}]);
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+
