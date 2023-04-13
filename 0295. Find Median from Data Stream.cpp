18bab57f10f1faaa6b4bf683582f5b373c759e1d
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-04-13 23:32:38 +0530

    Update 0295. Find Median from Data Stream.cpp

--- 0295. Find Median from Data Stream.cpp
+++ 0295. Find Median from Data Stream.cpp
@@ -27,3 +27,36 @@
 
 
 
+class MedianFinder {
+public:
+    priority_queue<int> decreasing;
+    priority_queue<int, vector<int>, greater<int>> increasing;
+    bool evn = true;
+    
+    MedianFinder() {
+    }
+    void addNum(int num) 
+    {
+        evn =!evn;
+        if(!evn)
+        {
+        decreasing.push(num);
+        increasing.push(decreasing.top());
+        decreasing.pop();
+        }
+        else
+        {
+              increasing.push(num);
+              decreasing.push(increasing.top());
+              increasing.pop();
+        }
+      
+    }
+    double findMedian() {
+        if (!evn) return increasing.top();
+        return (decreasing.top() + increasing.top()) / 2.0;
+    }
+};
+
+
+
