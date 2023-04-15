91c9bf5cd127ae329ad4e5ceae27abec6b94662e
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-04-15 23:38:32 +0530

    Update 2447. Number of Subarrays With GCD Equal to K.cpp

--- 2447. Number of Subarrays With GCD Equal to K.cpp
+++ 2447. Number of Subarrays With GCD Equal to K.cpp
@@ -18,3 +18,27 @@
 */
 
 
+
+class Solution {
+public:
+    int subarrayGCD(vector<int>& nums, int k)
+    {
+        int count = 0;
+
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            int currGCD = 0;
+            for(int j = i ; j < nums.size(); j++)
+            {
+                currGCD = gcd(currGCD, nums[j]);
+                
+                if (currGCD == k) 
+                    count++;
+            }
+        }
+
+        return count;
+    }
+};
+
+
