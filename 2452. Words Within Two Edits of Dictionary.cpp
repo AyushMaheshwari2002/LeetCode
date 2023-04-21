02663a20687afbc350ddd45de914a036af0f7312
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-04-21 22:03:42 +0530

    Update 2452. Words Within Two Edits of Dictionary.cpp

--- 2452. Words Within Two Edits of Dictionary.cpp
+++ 2452. Words Within Two Edits of Dictionary.cpp
@@ -23,3 +23,37 @@
 
 
 
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) 
+    {
+        vector<string> ans;
+    
+        for(int i = 0; i < queries.size(); i++)
+        {
+            for(int j = 0; j < dictionary.size(); j++)
+            {
+                int count = 0;
+
+                for(int k = 0; k < queries[0].size(); k++)
+                {
+                    if(queries[i][k] != dictionary[j][k]) 
+                    {
+                        count++;
+                    }
+                }
+                if(count <= 2)
+                {
+                    ans.push_back(queries[i]);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+
