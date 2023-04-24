ba8908f42975f5435edca7e5e46c3b6ed232ad2c
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-04-24 23:52:07 +0530

    Update 0224. Basic Calculator.cpp

--- 0224. Basic Calculator.cpp
+++ 0224. Basic Calculator.cpp
@@ -16,3 +16,50 @@
 */
 
 
+
+class Solution {
+public:
+    int calculate(string s) 
+    {
+        int n = s.size();
+        stack<int> st;
+        int sign =  1, sum = 0;
+        
+        for(int i =0 ; i<n; ++i)
+        {
+            if(s[i] >= '0' and s[i] <= '9')
+            {
+                int num = 0;
+                while(s.size() > i and s[i] >= '0' and s[i] <= '9')
+                {
+                    num = num * 10 + (s[i] - '0');
+                    ++i;
+                }
+                sum += num * sign;
+                --i;
+            }
+            else if(s[i] == '+')
+                sign = 1;
+            else if(s[i] == '-')
+                sign = -1;
+            else if(s[i] == '(')
+            {
+                st.push(sum);
+                st.push(sign);
+                sum = 0;
+                sign = 1;
+            }
+            else if(s[i] == ')')
+            {
+                sum = st.top() * sum;
+                st.pop();
+                sum += st.top();
+                st.pop();
+            }
+        }
+        return sum;
+    }
+};
+
+
+
