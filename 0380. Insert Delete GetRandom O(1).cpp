26d3e4594d35b704278ae5f480617c36ade8a8cb
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-06-14 23:40:05 +0530

    Update 0380. Insert Delete GetRandom O(1).cpp

--- 0380. Insert Delete GetRandom O(1).cpp
+++ 0380. Insert Delete GetRandom O(1).cpp
@@ -26,3 +26,41 @@
 */
 
 
+class RandomizedSet {
+public:
+    unordered_map<int, int> mp;
+    vector<int> v;
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(mp.find(val) == mp.end()) 
+        {
+            v.push_back(val) ;
+            mp[val] = v.size()-1;
+            return true;
+        }
+        return false;
+    }
+    
+    bool remove(int val) {
+        if(mp.find(val) == mp.end()) 
+            return false;
+        
+        int last = v.back();
+        mp[last] = mp[val];
+        v[mp[val]] = last;
+        v.pop_back();
+        mp.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return v[rand()%v.size()];
+    }
+};
+
+
+
+
