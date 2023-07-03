761b81b7d154ca78b6c81882f774ad5713bea518
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-07-03 23:51:23 +0530

    Update 0404. Sum of Left Leaves.cpp

--- 0404. Sum of Left Leaves.cpp
+++ 0404. Sum of Left Leaves.cpp
@@ -1,4 +1,4 @@
-htyhyhnhghgggfb/*  Given the root of a binary tree, return the sum of all left leaves.
+ghfhtyhyhnhghgggfb/*  Given the root of a binary tree, return the sum of all left leaves.
     A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
 
           Example 1:
