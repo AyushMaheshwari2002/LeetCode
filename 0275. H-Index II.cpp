199c869b0c747857e1a5163efe82a830eac1c346
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-07-08 23:31:31 +0530

    Update 0275. H-Index II.cpp

--- 0275. H-Index II.cpp
+++ 0275. H-Index II.cpp
@@ -16,3 +16,21 @@
 */
 
 
+
+class Solution {
+public:
+     int hIndex(vector<int>& citations)
+    {
+        int i = 0, j = citations.size() - 1;
+        
+        while(j >= 0 && citations[j] > i) {
+            i++;
+            j--;
+        }
+        
+        return i;
+    }
+};
+
+
+
