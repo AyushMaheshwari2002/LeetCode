4331735b3b114d465bf5855e530f5ad07355fbee
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-07-08 23:27:59 +0530

    Update 0274. H-Index.cpp

--- 0274. H-Index.cpp
+++ 0274. H-Index.cpp
@@ -14,3 +14,22 @@
 */
 
 
+class Solution {
+public:
+    int hIndex(vector<int>& citations)
+    {
+        sort(citations.begin(), citations.end());
+        int i = 0, j = citations.size() - 1;
+
+        while(j >= 0 && citations[j] > i) {
+            i++;
+            j--;
+        }
+        
+        return i;
+    }
+};
+
+
+
+
