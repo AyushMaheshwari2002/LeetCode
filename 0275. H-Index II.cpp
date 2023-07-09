f18398fb8352338629f90d484892d8baddd61c5b
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-07-09 23:40:32 +0530

    Update 0275. H-Index II.cpp

--- 0275. H-Index II.cpp
+++ 0275. H-Index II.cpp
@@ -1,4 +1,4 @@
-jjnnk/*  Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper and citations is sorted in
+jjjjnnk/*  Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper and citations is sorted in
     ascending order, return the researcher's h-index.
     According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at 
     least h papers that have each been cited at least h times.
