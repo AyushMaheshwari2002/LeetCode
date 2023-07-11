f03af7d5e2dd6104a41c15376a33846d9a0442aa
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-07-11 22:53:38 +0530

    Update 0451. Sort Characters By Frequency.cpp

--- 0451. Sort Characters By Frequency.cpp
+++ 0451. Sort Characters By Frequency.cpp
@@ -1,4 +1,4 @@
-cmfe/*  Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in 
+/*  Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in 
     the string.
     Return the sorted string. If there are multiple answers, return any of them.
 
@@ -20,6 +20,40 @@ cmfe/*  Given a string s, sort it in decreasing order based on the frequency of 
                 Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
                 Note that 'A' and 'a' are treated as two different characters.
 */
+
+
+
+class Solution {
+public:
+    string frequencySort(string s)
+    {
+        map<int,int> m;
+        
+        for(int i = 0; i < s.length(); i++)
+            m[s[i]]++;
+        
+        vector<pair<int , char>> v;
+        for(auto i : m)
+        {
+            v.push_back({i.second , i.first});    // in vector we have first store the frequency then no. b'coz we have to sort the vector
+                                                  // on the basis of increasing frequency
+        }
+        
+        sort(v.begin() , v.end());
+        reverse(v.begin() , v.end());
+        
+        string ans;
+        for(int i = 0; i < v.size(); i++)
+	    { 
+            int k = v[i].first;              // store frequency of each characters
+            
+            while(k--)
+		        ans += v[i].second;          // add each char by its frequency count
+	    }
+        
+        return ans;   
+    }
+};
 
 
 
