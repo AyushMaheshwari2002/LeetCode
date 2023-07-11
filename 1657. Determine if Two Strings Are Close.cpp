0cfe6aa5ffb298aed0316a277286875893e1f795
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-07-11 22:49:04 +0530

    Update 1657. Determine if Two Strings Are Close.cpp

--- 1657. Determine if Two Strings Are Close.cpp
+++ 1657. Determine if Two Strings Are Close.cpp
@@ -29,3 +29,43 @@
 
 
 
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) 
+    {
+        if(word1.size() != word2.size())
+            return false;
+
+        map<char,int> m1, m2;
+
+        for(int i = 0; i < word1.size(); i++)
+        { 
+            m1[word1[i]]++;
+            m2[word2[i]]++;
+        }
+        
+        for(int i = 0; i < word1.size(); i++) {
+            if(!m2[word1[i]])
+            {
+                return false;
+            }
+        }
+
+        vector<int> v1, v2;
+
+        for(auto i : m1)
+            v1.push_back(i.second);
+
+        for(auto i : m2)
+            v2.push_back(i.second);
+
+        sort(v1.begin(), v1.end());
+        sort(v2.begin(), v2.end());
+        
+        if(v1 == v2)
+            return true;
+        return false;
+    }
+};
+
+
