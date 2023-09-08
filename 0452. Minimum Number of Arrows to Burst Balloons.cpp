ee73432135e595052ed4a245220b836117d1ea66
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-09-08 23:34:47 +0530

    Update 0452. Minimum Number of Arrows to Burst Balloons.cpp

--- 0452. Minimum Number of Arrows to Burst Balloons.cpp
+++ 0452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -1,4 +1,4 @@
-nbf/*  There are some spherical balloons taped onto a flat wall that represents the XY-plane. The balloons are represented as a 2D integer array points where
+vdnbf/*  There are some spherical balloons taped onto a flat wall that represents the XY-plane. The balloons are represented as a 2D integer array points where
     points[i] = [xstart, xend] denotes a balloon whose horizontal diameter stretches between xstart and xend. You do not know the exact y-coordinates of the
     balloons.
     Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis. A balloon with xstart and xend is burst 
