3c2ff371b51552b0ef13d5e3ae8c761e17c5e399
Ayush Maheshwari <90818051+AyushMaheshwari2002@users.noreply.github.com>
2023-09-10 23:40:57 +0530

    Update 1833. Maximum Ice Cream Bars.cpp

--- 1833. Maximum Ice Cream Bars.cpp
+++ 1833. Maximum Ice Cream Bars.cpp
@@ -1,6 +1,6 @@
 /*  It is a sweltering summer day, and a boy wants to buy some ice cream bars.
-    At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants 
-    to buy as many ice cream bars as possible. 
+    At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The 
+    boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
     Note: The boy can buy the ice cream bars in any order.
     Return the maximum number of ice cream bars the boy can buy with coins coins.
     You must solve the problem by counting sort.
@@ -22,4 +22,24 @@
 */
 
 
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) 
+    {
+        int count = 0;
+        sort(costs.begin(),costs.end());
+
+        for(int i = 0; i < costs.size(); i++)
+        {
+            if(costs[i] > coins)
+                break;
+            
+            coins -= costs[i];
+            count++;
+        }
+
+        return count;
+    }
+};
 
